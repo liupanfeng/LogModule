956eae40255fafdaf80691f96c0cad8f2b1d9791
elongxu <xuyilong@chinasofti.com>
2017-04-20 10:53:10 +0800

    first commit

---
+++ app/src/main/java/com/example/lpf/logmodule/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.lpf.logmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 日志模块总结
+ * 目的：记录日志模块，更好的查找bug并解决问题，整理一整套解决方案。
+ * 描述：此工程专门用于总结日志体系，不做其他方面的事情。
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/lpf/logmodule/app/MyApp.java
@@ -0,0 +1,15 @@
+package com.example.lpf.logmodule.app;
+
+import android.app.Application;
+
+/**
+ * Created by lpf on 2017/4/20.
+ * 程序入口
+ */
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
